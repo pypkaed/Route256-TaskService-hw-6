a4cf3f904c8ae35c6449255596d3261669ce52ec
pypkaed <pypka.eduardovna@yandex.ru>
2023-10-31 14:50:53 +0300

    [CSJHW-6]: make one query in migration

--- src/HomeworkApp.Dal/Migrations/20230917143700_AddTaskCommentV2Type.cs
+++ src/HomeworkApp.Dal/Migrations/20230917143700_AddTaskCommentV2Type.cs
@@ -7,31 +7,23 @@ public class AddTaskCommentV2Type : Migration
 {
     public override void Up()
     {
-        const string addFirstColumnSql = @"
+        const string addColumnsSql = @"
 ALTER TABLE task_comments
-ADD COLUMN modified_at timestamp with time zone
-;";
-        const string addSecondColumnSql = @"
-ALTER TABLE task_comments
+ADD COLUMN modified_at timestamp with time zone,
 ADD COLUMN deleted_at timestamp with time zone
 ;";
         
-        Execute.Sql(addFirstColumnSql);
-        Execute.Sql(addSecondColumnSql);
+        Execute.Sql(addColumnsSql);
     }
 
     public override void Down()
     {
-        const string deleteFirstColumnSql = @"
+        const string deleteColumnsSql = @"
 ALTER TABLE task_comments
-DROP COLUMN modified_at
-;";
-        const string deleteSecondColumnSql = @"
-ALTER TABLE task_comments
+DROP COLUMN modified_at,
 DROP COLUMN deleted_at
 ;";
         
-        Execute.Sql(deleteFirstColumnSql);
-        Execute.Sql(deleteSecondColumnSql);
+        Execute.Sql(deleteColumnsSql);
     }
 }
